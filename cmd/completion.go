e87e4b3585bb2c7e34dcbc4774494e46d74b72e3
Pasquale Sociale <pasquale.sociale@gmail.com>
2026-04-23 10:39:44 +0200

    feat: add completions

---
+++ cmd/completion.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion <shell>",
+	Short: "Generate shell completions",
+	Long: `Generate shell completion scripts for dot.
+
+Usage:
+  dot completion bash   > ~/.local/share/bash-completion/completions/dot
+  dot completion zsh    > "${fpath[1]}/_dot"
+  dot completion fish   > ~/.config/fish/completions/dot.fish
+`,
+	Args:      cobra.ExactArgs(1),
+	ValidArgs: []string{"bash", "zsh", "fish", "powershell"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletion(os.Stdout)
+		case "zsh":
+			return rootCmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			return rootCmd.GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			return rootCmd.GenPowerShellCompletionWithDesc(os.Stdout)
+		default:
+			return fmt.Errorf("unsupported shell: %s", args[0])
+		}
+	},
+}
